051d4a7366b6507f688f6c671746d67829257d0b
skyaironline <aldo@batamon.asia>
2025-06-29 11:26:15 +0700

    type hint

--- script/deploy.py
+++ script/deploy.py
@@ -1,14 +1,15 @@
 from src import favorites
+from moccasin.boa_tools import VyperContract
 
-def deploy_favorites():
-    favorites_contract = favorites.deploy()
-    starting_number = favorites_contract.retrieve()
+def deploy_favorites() -> VyperContract:
+    favorites_contract: VyperContract = favorites.deploy()
+    starting_number: int = favorites_contract.retrieve()
     print(f"Starting number: {starting_number}")
 
     favorites_contract.store(10)
-    updated_number = favorites_contract.retrieve()
+    updated_number: int = favorites_contract.retrieve()
     print(f"Updated number: {updated_number}")
     return favorites_contract
           
-def moccasin_main():
+def moccasin_main() -> VyperContract:
     deploy_favorites()
