d65aba0a85f2f7d0c2f7d7c7ab23c190fb7fa59c
skyaironline <aldo@batamon.asia>
2025-06-29 11:39:58 +0700

    Make config deploy to sepolia

--- script/deploy.py
+++ script/deploy.py
@@ -1,5 +1,6 @@
 from src import favorites
 from moccasin.boa_tools import VyperContract
+from moccasin.config import get_active_network
 
 def deploy_favorites() -> VyperContract:
     favorites_contract: VyperContract = favorites.deploy()
@@ -9,6 +10,11 @@ def deploy_favorites() -> VyperContract:
     favorites_contract.store(10)
     updated_number: int = favorites_contract.retrieve()
     print(f"Updated number: {updated_number}")
+
+    active_network = get_active_network()
+    if active_network.has_explorer():
+        result = active_network.moccasin_verify(favorites_contract)
+        result.wait_for_verification()
     return favorites_contract
           
 def moccasin_main() -> VyperContract:
